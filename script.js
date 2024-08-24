48fd2984e6b6e7911c30b33a605ccd498b278b7f
mja1337 <76176518+mja1337@users.noreply.github.com>
2024-08-24 10:30:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(files => {
             const soundboard = document.getElementById('soundboard');
 
+            // Create a button for each sound file
             files.forEach(filename => {
                 const buttonLabel = filename.replace(/_/g, ' ').replace('.mp3', '');
                 const button = document.createElement('button');
@@ -17,6 +18,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 soundboard.appendChild(button);
             });
+
+            // Create a "Random" button
+            const randomButton = document.createElement('button');
+            randomButton.classList.add('button');
+            randomButton.innerText = 'Random';
+
+            randomButton.addEventListener('click', () => {
+                const randomFile = files[Math.floor(Math.random() * files.length)];
+                const audio = new Audio(`bauersamples/${randomFile}`);
+                audio.play();
+            });
+
+            soundboard.appendChild(randomButton);
         })
         .catch(error => console.error('Error loading files:', error));
 });
